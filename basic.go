5d6814e66e5b0a5535214872986f992fce854805
sagayosa <gyjgrteen@gmail.com>
2024-03-27 19:02:29 +0800

    add basic method

--- basic.go
+++ basic.go
@@ -14,3 +14,23 @@ func Request[T any](method, URL string, opt *Option) T {
 	json.Unmarshal(bts, &result)
 	return result
 }
+
+func Get[T any](URL string, opts ...OptionFunc) T {
+	opt := NewOption(opts...)
+	return Request[T]("GET", URL, opt)
+}
+
+func Post[T any](URL string, opts ...OptionFunc) T {
+	opt := NewOption(opts...)
+	return Request[T]("Post", URL, opt)
+}
+
+func Put[T any](URL string, opts ...OptionFunc) T {
+	opt := NewOption(opts...)
+	return Request[T]("Put", URL, opt)
+}
+
+func Delete[T any](URL string, opts ...OptionFunc) T {
+	opt := NewOption(opts...)
+	return Request[T]("Delete", URL, opt)
+}
