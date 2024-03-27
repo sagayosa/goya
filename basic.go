7b475078e4d7f1d122274284f6fe75a3e526a7ae
sagayosa <gyjgrteen@gmail.com>
2024-03-27 18:50:37 +0800

    add Request and RequestRaw

--- basic.go
+++ basic.go
@@ -1,7 +1,16 @@
 package goya
 
-import "net/http"
+import "encoding/json"
 
-func RequestRaw(method, URL string, opt *Option) *http.Response {
-	return NewRequestClient(method, URL, opt, nil).Do()
+// RequestRaw returns the *http.Response after the request and no reads were made
+func RequestRaw(method, URL string, opt *Option) *Response {
+	return NewResponse(NewRequestClient(method, URL, opt, nil).Do())
+}
+
+// Request returns the instance of the given T after JSON parsing
+func Request[T any](method, URL string, opt *Option) T {
+	bts, _ := NewResponse(NewRequestClient(method, URL, opt, nil).Do()).Bytes()
+	var result T
+	json.Unmarshal(bts, &result)
+	return result
 }
