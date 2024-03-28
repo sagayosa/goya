b29308d7b8610d28c990cd4c3163e57e85175392
sagayosa <gyjgrteen@gmail.com>
2024-03-28 11:40:09 +0800

    improve TestGet

--- example_test.go
+++ example_test.go
@@ -10,6 +10,15 @@ func TestGet(t *testing.T) {
 		t.Fatal("Get got nil")
 	}
 	if resp.URL != StringPlus(testURL, "?temp=2") {
-		t.Errorf("resp.URL got %v but want %v", resp.URL, testURL)
+		t.Errorf("resp.URL got %v but want %v", resp.URL, StringPlus(testURL, "?temp=2"))
+	}
+
+	req := struct {
+		Name string `json:"name"`
+		ID   int    `json:"id"`
+	}{"Hello", 3306}
+	resp2 := Get[BasicGetResponse](testURL, req)
+	if resp2.URL != StringPlus(testURL, "?name=Hello&id=3306") && resp2.URL != StringPlus(testURL, "?id=3306&name=Hello") {
+		t.Errorf("resp.URL got %v but want %v", resp2.URL, StringPlus(testURL, "?name=Hello&id=3306"))
 	}
 }
