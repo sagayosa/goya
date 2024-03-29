f4531c0e66d6ad43039dd50134e80e904fc9c11b
sagayosa <gyjgrteen@gmail.com>
2024-03-29 12:26:14 +0800

    rm junk

--- option.go
+++ option.go
@@ -1,20 +1,6 @@
 package goya
 
 type Option struct {
-	// Json can be used then you want to send JSON within the request body
-	// Json should be map or struct
-	// Json will convert into the body
-	// Json any
-
-	// Params must be map or struct
-	// Params will convert into the URL as the query argument
-	// Params any
-
-	// FormData must be map or struct
-	// FormData will convert into the body
-	// FormData any
-
-	// Headers map[string]string
 	before []BeforeBuildFunc
 	after  []AfterBuildFunc
 }
--- option_func.go
+++ option_func.go
@@ -10,32 +10,6 @@ import (
 	"reflect"
 )
 
-// type OptionFunc func(opt *Option)
-
-// WithJson will add data to the request body
-// and set the Content-Type to application/json
-// func WithJson(data any) OptionFunc {
-// 	return func(opt *Option) {
-// 		opt.Json = data
-// 	}
-// }
-
-// WithParams will add params to the request URL
-// params must be a structure or a map
-// func WithParams(params any) OptionFunc {
-// 	return func(opt *Option) {
-// 		opt.Params = params
-// 	}
-// }
-
-// WithForm will add data to the request body
-// and set the Content-Type to multipart/form-data
-// func WithForm(data any) OptionFunc {
-// 	return func(opt *Option) {
-// 		opt.FormData = data
-// 	}
-// }
-
 type OptionFunc func(any) (BeforeBuildFunc, AfterBuildFunc)
 type BeforeBuildFunc func(b *RequestBuider)
 type AfterBuildFunc func(req *http.Request)
